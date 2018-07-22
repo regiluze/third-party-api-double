8e83b93a9a5fdce0bb7187a40f66a788f6d9d236
regiluze <regiluze@hotmail.com>
2018-07-22 23:39:12 +0200

    service test suite scaffolding added

---
+++ service_test.go
@@ -0,0 +1,24 @@
+package service_test
+
+import (
+	_ "fmt"
+	. "github.com/onsi/ginkgo"
+	_ "github.com/onsi/gomega"
+)
+
+const ()
+
+var _ = Describe("Third party API double service specs", func() {
+	var ()
+
+	BeforeEach(func() {
+	})
+
+	Describe("foo", func() {
+		Context("var", func() {
+			It("kk", func() {
+			})
+		})
+	})
+
+})
